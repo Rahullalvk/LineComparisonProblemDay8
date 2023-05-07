e1741e8a27098890487e0979b9a404a928e87d01
Rahullal <Rahullal432@gmail.com>
2023-05-07 14:28:21 +0530

    compare two lines based on the end points

--- LineComparisonProblemDay8/lineComparison.cs
+++ LineComparisonProblemDay8/lineComparison.cs
@@ -12,18 +12,38 @@ namespace LineComparisonProblemDay8
         {
             Console.WriteLine("Consider lines a & b");
             Console.WriteLine("Enter the cordinates (x1, y1) and (x2, y2):");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("x1= :" + x1);
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("y1= :" + y1);
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("x2= :" + x2);
-            int y2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("y2= :" + y2);
-            float length = 0.0f;
-            length = (float)Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("The length of the line is =" + length);
+            int x1a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x1= :" + x1a);
+            int y1a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1= :" + y1a);
+            int x2a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x2= :" + x2a);
+            int y2a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y2= :" + y2a);
+            float length1 = (float)Math.Sqrt((x2a - x1a) ^ 2 + (y2a - y1a) ^ 2);
+            Console.WriteLine("The length of first line =" + length1);
 
+            Console.WriteLine("Enter the cordinates (x1, y1) and (x2, y2):");
+            int x1b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x1= :" + x1b);
+            int y1b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1= :" + y1b);
+            int x2b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x2= :" + x2b);
+            int y2b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y2= :" + y2b);
+            float length2 = (float)Math.Sqrt((x2b - x1b) ^ 2 + (y2b - y1b) ^ 2);
+            Console.WriteLine("The length of second line =" + length2);
+
+            if (length1 == length2)
+            {
+                Console.WriteLine("Two lines a and b are equal");
+
+            }
+            else
+            {
+                Console.WriteLine("Two lines a and b are not equal");
+            }
         }
     }
 }
